d3f61cc61c9d933b48341210149ad16544fdff2f
naughtyfiddle <ctdinh93@gmail.com>
2017-07-29 22:28:33 -0500

    update about me

--- src/containers/desktop/apps/about-me.jsx
+++ src/containers/desktop/apps/about-me.jsx
@@ -8,10 +8,10 @@ export default function AboutMe(props) {
 			</h3>
 			<div className="about-me-content">
 				<p>
-					Thanks for checking out my website! My name is Christian Dinh.
-					I'm a full-stack software engineer with a propensity for the front end.
-					I use a lot of different technologies in my day-to-day life, but this website
-					in particular is built with:
+					Thanks for checking out my website! My name is Christian Dinh
+					and I'm a software engineer living in Austin TX.
+					I use a lot of different technologies in my day-to-day life, but this
+					particular website is built with:
 				</p>
 				<ul>
 					<li>React</li>
@@ -21,29 +21,24 @@ export default function AboutMe(props) {
 					<li>Less</li>
 				</ul>
 				<p>
-					And that's about it! No back end in this case, the whole site is
-					hosted via github pages. Unsurprisingly, source code can be viewed
-					at <a href="https://github.com/naughtyfiddle/portfolio-v3">this site's github repository</a>.
+					And that's about it! Source code can be
+					viewed <a href="https://github.com/naughtyfiddle/portfolio-v3">on Github</a>.
 				</p>
 				<p>
-					I am also reasonably well-versed in accessibility techniques. You might
-					notice that this site is fully keyboard and screen-reader accessible, and
-					that is quite on purpose. Accessibility is great and makes the internet a
-					better place for everyone, so if you haven't made your apps accessible
-					you should definitely do that!
+					This site is intended to be fully keyboard and screen-reader accessible.
+					Accessibility is great and makes the internet a better place for
+					everyone, so if you haven't made your apps accessible you should
+					definitely do that!
 				</p>
 				<p>
-					Thanks for reading, that's all I have to say! If you would like to contact me,
-					you've got a few options:
+					Thanks for reading this far! If you would like to contact me,
+					you have a few options:
 				</p>
 				<ul>
 					<li>email</li>
 					<li>linkedin</li>
 					<li>github</li>
 				</ul>
-				<p>
-					Enjoy your stay, I hope you have as much fun visiting this site as I had making it :D
-				</p>
 			</div>
 		</div>
 	);
