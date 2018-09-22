2d387e2bddafaacad580e1966cfbe1b4bc50cf40
naughtyfiddle <ctdinh93@gmail.com>
2018-09-22 17:20:23 -0500

    update tooling

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = function(api) {
+	api.cache.forever();
+
+	return {
+		plugins: [
+			'@babel/proposal-class-properties',
+			'@babel/proposal-object-rest-spread'
+		],
+		presets: [
+			['@babel/env', {modules: false}],
+			'@babel/react'
+		]
+	};
+};
