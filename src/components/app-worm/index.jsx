acfd9fdc4f8abd88adb03556929277149c6b750b
bass-dandy <ctdinh93@gmail.com>
2018-09-30 15:54:24 -0500

    add controls and desc to worm app

--- src/components/app-worm/index.jsx
+++ src/components/app-worm/index.jsx
@@ -18,10 +18,40 @@ export default class Worm extends React.Component {
 
 	render() {
 		return (
-			<canvas
-				className="worm-app"
-				ref={(e) => { this.canvas = e; }}
-			/>
+			<div className="worm-app">
+				<canvas
+					className="worm-canvas"
+					ref={(e) => { this.canvas = e; }}
+				/>
+				<div className="desktop-controls">
+					<div className="worm-desc">
+						<div>
+							A game by Christian Dinh
+						</div>
+						<div>
+							<a href="https://github.com/bass-dandy/w0rm" target="blank">
+								github.com/bass-dandy/w0rm
+							</a>
+						</div>
+					</div>
+					<table className="controls-table">
+						<tbody>
+							<tr>
+								<td>arrow keys</td>
+								<td>move</td>
+							</tr>
+							<tr>
+								<td>z, x</td>
+								<td>shoot portals</td>
+							</tr>
+							<tr>
+								<td>esc</td>
+								<td>pause</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 		);
 	}
 }
