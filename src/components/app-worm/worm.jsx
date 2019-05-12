5a3b3ed90b22818c2b6efaeb7d00ddec88e20164
bass-dandy <ctdinh93@gmail.com>
2019-05-12 18:07:50 -0500

    fix w0rm play/pause bugs, render score

--- src/components/app-worm/worm.jsx
+++ src/components/app-worm/worm.jsx
@@ -5,15 +5,26 @@ import styles from './worm.module.css';
 
 export default class Worm extends React.Component {
 
-	state = { game: null }
+	state = {
+		game: null,
+		score: 0
+	}
 
 	componentDidMount() {
-		this.setState({ game: new WormGame(this.canvas) });
+		const game = new WormGame(
+			this.canvas,
+			(score) => this.setState({score})
+		);
+		game.enableKeyboardControls();
+		this.setState({game}) ;
 	}
 
 	componentDidUpdate(prevProps) {
 		if (prevProps.isFocused && !this.props.isFocused) {
 			this.state.game.pause();
+			this.state.game.disableKeyboardControls();
+		} else if (!prevProps.isFocused && this.props.isFocused) {
+			this.state.game.enableKeyboardControls();
 		}
 	}
 
@@ -24,6 +35,12 @@ export default class Worm extends React.Component {
 	render() {
 		return (
 			<div className={styles.worm}>
+				<div className={styles.scoreContainer}>
+					Score:
+					<div className={styles.score}>
+						{this.state.score * 100}
+					</div>
+				</div>
 				<canvas
 					className={styles.canvas}
 					ref={(e) => { this.canvas = e; }}
