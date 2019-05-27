e92b8a5338b99a8b7190b889b1ebb3060b23b65c
bass-dandy <ctdinh93@gmail.com>
2019-05-27 17:30:53 -0500

    update resume

--- src/components/app-resume/resume.jsx
+++ src/components/app-resume/resume.jsx
@@ -1,17 +1,12 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
 import {download} from 'src/util';
 import MenuBar from '../menu-bar';
 
-import resumeImg from 'static/img/resume/resume.jpg';
 import resumePdf from 'static/img/resume/resume.pdf';
 import styles from './resume.module.css';
 
-const DEFAULT_ZOOM = 550;
-const MIN_ZOOM = 250;
-const MAX_ZOOM = 850;
-
 export default function Resume() {
-	const [zoom, setZoom] = useState(DEFAULT_ZOOM);
+	const iframe = useRef(null);
 
 	return (
 		<div className={styles.resume}>
@@ -21,35 +16,24 @@ export default function Resume() {
 						{
 							label: 'Download pdf',
 							onClick: () => download(resumePdf)
-						}, {
-							label: 'Download jpg',
-							onClick: () => download(resumeImg)
+						},
+						{
+							label: 'Print',
+							onClick: () => iframe.current.contentWindow.print()
 						}
 					],
 					View: [
 						{
-							label: 'Actual Size',
-							onClick: () => setZoom(DEFAULT_ZOOM),
-							disabled: zoom === DEFAULT_ZOOM
-						}, {
-							label: 'Zoom In',
-							onClick: () => setZoom(zoom + 150),
-							disabled: zoom >= MAX_ZOOM
-						}, {
-							label: 'Zoom Out',
-							onClick: () => setZoom(zoom - 150),
-							disabled: zoom <= MIN_ZOOM
+							label: 'Open in New Tab',
+							onClick: () => window.open(resumePdf)
 						}
 					]
 				}}
 			/>
 			<div className={styles.frame}>
-				<img
-					src={resumeImg}
-					alt="my resume"
-					style={{
-						width: zoom
-					}}
+				<iframe
+					src={resumePdf}
+					ref={iframe}
 				/>
 			</div>
 		</div>
