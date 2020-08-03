d05e9a56f95b282c939b43980ed7712fbbdd2c01
bass-dandy <ctdinh93@gmail.com>
2020-08-03 19:10:02 -0400

    add got damn phone, update cover art

--- src/components/app-media-player/songs.js
+++ src/components/app-media-player/songs.js
@@ -1,7 +1,8 @@
 import cover from 'static/music/cover.jpg';
-import track1 from 'static/music/daylight.mp3';
-import track2 from 'static/music/bass-dandy-desu.mp3';
-import track3 from 'static/music/hear-me-roar.mp3';
+import track1 from 'static/music/01 - Sunrise Bleeds Across Ocean Waves.mp3';
+import track2 from 'static/music/02 - Hajimemashite, Aratamemashite, Bass Dandy Desu.mp3';
+import track3 from 'static/music/03 - Got Damn Phone.mp3';
+import track4 from 'static/music/04 - Hear Me Roar.mp3';
 
 export default [
 	{
@@ -23,13 +24,21 @@ export default [
 		cover
 	},
 	{
+		title: 'Got Damn Phone',
+		artist: 'Bass Dandy',
+		album: 's/t',
+		length: '1:08',
+		track: '3',
+		src: track3,
+		cover
+	},
+	{
 		title: 'Hear Me Roar',
 		artist: 'Bass Dandy',
 		album: 's/t',
 		length: '5:02',
 		track: '3',
-		src: track3,
+		src: track4,
 		cover
 	}
 ];
-
